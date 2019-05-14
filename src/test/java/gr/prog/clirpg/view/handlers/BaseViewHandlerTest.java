1e863b05d4dd230b06b2e9d8332d930728107516
gr <gr@gr.gr>
2019-05-15 00:10:41 +0300

    tests

---
+++ src/test/java/gr/prog/clirpg/view/handlers/BaseViewHandlerTest.java
@@ -0,0 +1,36 @@
+package gr.prog.clirpg.view.handlers;
+
+import gr.prog.clirpg.model.characters.Hero;
+import gr.prog.clirpg.services.CurrentHero;
+import gr.prog.clirpg.services.HeroService;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class BaseViewHandlerTest {
+	private HeroService heroService = Mockito.mock(HeroService.class, Mockito.RETURNS_DEEP_STUBS);
+	private CurrentHero currentHero = Mockito.mock(CurrentHero.class, Mockito.RETURNS_DEEP_STUBS);
+	private List<BaseViewHandler> viewHandlers = Arrays.asList(
+			new CreateHeroHandler(heroService, currentHero),
+			new MainMenuHandler(heroService, currentHero),
+			new WelcomeHandler(),
+			new WorldMapHandler(currentHero),
+			new LoadGameHandler(heroService, currentHero)
+	);
+
+	//
+	@Test
+	public void getTextPresent() {
+//		Mockito.when(currentHero.getHero()).thenReturn(Mockito.mock(Hero.class, Mockito.RETURNS_DEEP_STUBS));
+		for (BaseViewHandler viewHandler : viewHandlers) {
+			String textPresent = viewHandler.getTextPresent();
+			Assert.assertNotNull(textPresent);
+			Assert.assertFalse(textPresent.isEmpty());
+
+		}
+
+	}
+}
